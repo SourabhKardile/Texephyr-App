8fa338a31240188a653a29a883cb30755209e1e0
creepymarshmallow117 <47560755+creepymarshmallow117@users.noreply.github.com>
2023-01-07 16:13:01 +0530

    test

--- components/login/home_screen.js
+++ components/login/home_screen.js
@@ -1,9 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer(){
+  return(
+    <Drawer.Navigator>
+      <Drawer.Screen name="Forgot Password" component={ForgotPassword}/>
+      <Drawer.Screen name="Logout" component={Logout}/>
+    </Drawer.Navigator>
+  );
+}
 
 const Home_screen = () => {
   return (
     <View>
+      <MyDrawer/>
       <Text>HomeScreen</Text>
     </View>
   )
