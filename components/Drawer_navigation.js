04df0c1460db5289de1068321edcd93b23fab2f4
Sourabh Kardile <SOURABHKARDILE47@GMAIL.COM>
2023-01-07 19:09:44 +0530

    reset

--- components/Drawer_navigation.js
+++ components/Drawer_navigation.js
@@ -6,15 +6,19 @@ import {
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
-
 import { NavigationContainer } from '@react-navigation/native';
 import Home_screen from './login/Home_screen';
+import Reset from './login/Reset_Password';
+
+
+
 const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
   return (
     <Drawer.Navigator >
       <Drawer.Screen name="Home" component={Home_screen}/>
+      <Drawer.Screen name="Reset Password" component={Reset}/>
     </Drawer.Navigator>
     
   );
--- components/login/home_screen.js
+++ components/login/home_screen.js
@@ -3,10 +3,30 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Home_screen = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator 
+    screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+    >
     <Stack.Screen name="Event" component={Event}  />
     <Stack.Screen name="Register" component={Register} />
     <Stack.Screen name="My Status" component={MyStatus} />
