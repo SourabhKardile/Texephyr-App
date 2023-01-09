cf4f522bf2e9e1ee3a3eb3c2e1277b0ed21d0346
Sourabh Kardile <SOURABHKARDILE47@GMAIL.COM>
2023-01-09 12:54:42 +0530

    records

--- components/login/home_screen.js
+++ components/login/home_screen.js
@@ -7,7 +7,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import EventRegisteration from './EventRegisteration';
 import { element } from 'prop-types';
 import EventList from './Event';
-import Records from './Records';
+import Records from './MainRecord';
 
 const Home_screen = () => {
   return (
