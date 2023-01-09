cc20889a797c15cdfe6c61c6b6dd6cb465b9935b
Sourabh Kardile <SOURABHKARDILE47@GMAIL.COM>
2023-01-09 11:52:59 +0530

    Update Records.js

--- components/login/Records.js
+++ components/login/Records.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button , StyleSheet, TouchableOpacity} from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default function Records() {
   return (
-    <View>
-      <Text>Records</Text>
+    <View style={{flex:1, flexDirection:'row'}}>
+    <ScrollView>
+    <View style={{width:'100%', justifyContent:'flex-start'}}>
+<TouchableOpacity style={styles.box}>
+    <Text style={{color:'white', fontSize:25,padding:17, fontWeight:'bold'}}>UPDATE</Text>
+</TouchableOpacity>
+<TouchableOpacity style={[styles.box, {backgroundColor:'green'}]}>
+    <Text style={{color:'white', fontSize:25,padding:17, fontWeight:'bold'}}>RECORDS</Text>
+</TouchableOpacity>
+<TouchableOpacity style={[styles.box, {backgroundColor:'red'}]}>
+    <Text style={{color:'white', fontSize:25,padding:17, fontWeight:'bold'}}>EVENT SALES</Text>
+</TouchableOpacity>
+<TouchableOpacity style={[styles.box, {backgroundColor:'yellow'}]}>
+    <Text style={{color:'white', fontSize:25,padding:17, fontWeight:'bold'}}>OVERALL</Text>
+</TouchableOpacity>
+    </View>
+    </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+    box:{
+         height: 160,
+          marginTop: 10,
+           backgroundColor:'#2196F3',
+            paddingTop:50
+    }
+})
